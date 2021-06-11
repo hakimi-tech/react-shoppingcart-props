5beacda9cc46a94d372e97699887b392a7152582
hakimi-tech <60336830+hakimi-tech@users.noreply.github.com>
2021-06-11 14:17:39 +0400

    Update cart.js

--- react-cart/src/components/cart.js
+++ react-cart/src/components/cart.js
@@ -8,6 +8,7 @@ function Cart(props) {
                 <img src={props.url} alt=""/>
                 <h4>{props.name}</h4>
                 <h5>{props.price}</h5>
+                <button className="button">Add To Cart</button>
             </div>
         </div>
     )
